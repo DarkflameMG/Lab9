d2a4bafa3e5cedec03b152e4c6d46801d8849d97
DarkflameMG <deknan70@gmail.com>
2021-01-08 15:53:59 +0700

    lab9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string text;
+    int count = 0;
+    double sum=0,sumpow = 0,number;
+    while(getline(source,text))
+    {
+        count++;
+        number =  atof(text.c_str());
+        sum += number;
+        sumpow += (number * number);
+    }
+    cout << "Number of data = " << count << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << '\n';
+    cout << "Standard deviation = " << sqrt((sumpow/count)-(sum/count)*(sum/count));
+}
